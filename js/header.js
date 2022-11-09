3221b0138c65701096d7ab74b5f6c422acc3c6e1
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2022-11-09 16:39:39 +0900

    on 2022年 11月  9日 水曜日 16:39:39 JST

---
+++ js/header.js
@@ -0,0 +1,10 @@
+
+document.write('<header>');
+document.write('<div id="title">よくわかる（かもしれない）音楽Ⅰ</div>');
+document.write('<ul id="headerButtonList">');
+document.write('	<li class="headerButton"><a href="./index.html">ホーム</a></li>');
+document.write('	<li class="headerButton"><a href="">商品ページ</a></li>');
+document.write('	<li class="headerButton"><a href="./artists.html">アーティスト紹介</a></li>');
+document.write('	<li class="headerButton"><a href="">コラム</a></li>');
+document.write('</ul>');
+document.write('</header>');
